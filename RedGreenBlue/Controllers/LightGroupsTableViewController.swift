766768352f1d694a1175c227595a4594fa2e905d
Tristan Griffin <tristangriffin@icloud.com>
2019-08-16 17:34:45 -0400

    groups tableView

--- RedGreenBlue/Controllers/LightGroupsTableViewController.swift
+++ RedGreenBlue/Controllers/LightGroupsTableViewController.swift
@@ -12,27 +12,55 @@ import SwiftyHue
 class LightGroupsTableViewController: UITableViewController {
     
     var rgbBridge: RGBHueBridge?
+    var groupIdentifiers: [String] = []
+    var groups: [String: Any] = [:]
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    override func viewDidAppear(_ animated: Bool) {
+    override func viewWillAppear(_ animated: Bool) {
         
-        print(rgbBridge)
+        guard let rgbBridge = rgbBridge else {
+            return
+        }
+        
+        let bridgeAccessConfig = BridgeAccessConfig(bridgeId: "BridgeId", ipAddress: rgbBridge.ipAddress, username: rgbBridge.username)
         
         let swiftyHue = SwiftyHue()
-        //swiftyHue.
+        swiftyHue.setBridgeAccessConfig(bridgeAccessConfig)
         
+        let resourceAPI = swiftyHue.resourceAPI
+        resourceAPI.fetchGroups({ (result) in
+            guard let groups = result.value else {
+                return
+            }
+            
+            self.groups = groups
+            
+            
+            for group in groups {
+                self.groupIdentifiers.append(group.key)
+                print("LightsGroupTableViewController: \(group.value.name)")
+            }
+            print("LightsGroupTableViewController: HERE")
+            self.tableView.reloadData()
+        })
     }
-}
-
-extension LightGroupsTableViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return groups.count
+    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "GroupsCellIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GroupsCellIdentifier") as! LightsGroupCustomCell
         
-        //cell.textLabel?.text =
+        let group = groups[groupIdentifiers[indexPath.row]] as! Group
         
+        print("LightsGroupTableViewController: \(group)")
+        
+        cell.label.text = group.name
+ 
         return cell
     }
 }
