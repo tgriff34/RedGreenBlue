cae6db5db791c43985ebe49aead4938e4eaf2c8e
griffind <griffindm@appstate.edu>
2019-08-15 20:23:19 -0400

    Initial Commit

---
+++ RedGreenBlue/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  RedGreenBlue
+//
+//  Created by Dana Griffin on 8/15/19.
+//  Copyright © 2019 Dana Griffin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
