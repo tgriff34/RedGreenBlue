5b39ff3d41db86beb38d1a650fd0d5642a44a03c
Tristan Griffin <tristangriffin@icloud.com>
2019-10-02 11:54:38 -0400

    change to shadow behind card view cells

--- RedGreenBlue/Main/Scenes/LightsSceneCustomCell.swift
+++ RedGreenBlue/Main/Scenes/LightsSceneCustomCell.swift
@@ -15,11 +15,9 @@ class LightSceneCustomCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         subView.layer.cornerRadius = 20.0
-        if self.traitCollection.userInterfaceStyle != .dark {
-            subView.layer.shadowColor = UIColor.gray.cgColor
-            subView.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
-            subView.layer.shadowOpacity = 0.7
-            subView.layer.shadowRadius = 4.7
-        }
+        subView.layer.shadowColor = UIColor.black.cgColor
+        subView.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
+        subView.layer.shadowOpacity = 0.34
+        subView.layer.shadowRadius = 4.3
     }
 }
